49dac43a5de0b0faa937f0632a425a488f4c4baa
Subramanya Talli <subramanya.talli@gmail.com>
2025-01-13 17:55:54 +0530

    Controller-Changes

--- backEnd/controller/controller.js
+++ backEnd/controller/controller.js
@@ -41,9 +41,47 @@ async function getOneBook(req, res) {
     }
 }
 
+async function UpdateBook(req, res) {
+    
+    try {
+        if(!req.body.title || !req.body.author || !req.body.publishYear)
+        {
+            return res.status(400).send({ message : "Send all the required details: title, author, Publish Year"})
+        }
+        const { id } = req.params;
+
+        const book = await Book.findById(id)
+        if(!book)
+        {
+            return res.status(404).send({message: "Book Not Found"});
+        }
+        return res.status(200).send({ message:"Books updated Successfully"})
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+async function deleteBook(req, res)
+{
+    try {
+        const { id } = req.params;
+        
+        const book = await Book.findByIdAndDelete(id);
+    
+        if (!book) {
+            return res.status(404).json({ message: "Book Not found"});
+        }
+        return res.status(200).send({ message : "Book Deleted Successfully"});
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
 
 module.exports = {
     newBookEntry,
     getAllBooks,
-    getOneBook
+    getOneBook,
+    UpdateBook,
+    deleteBook
 }
--- backEnd/routes/routes.js
+++ backEnd/routes/routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { newBookEntry, getAllBooks, getOneBook } = require("../controller/controller");
+const { newBookEntry, getAllBooks, getOneBook, UpdateBook, deleteBook } = require("../controller/controller");
 
 const routes = express.Router();
 
 routes.post("/newBook", newBookEntry);
 routes.get("/", getAllBooks);
 routes.get("/:id", getOneBook);
-
-
+routes.put("/:id", UpdateBook);
+routes.delete("/:id", deleteBook);
 
 module.exports = routes;
