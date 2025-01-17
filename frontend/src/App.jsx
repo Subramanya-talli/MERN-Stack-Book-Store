92609e27f20cc96972b2615bd45511bf96e52c52
Subramanya Talli <subramanya.talli@gmail.com>
2025-01-17 22:39:40 +0530

    fontend-changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import CreateBook from './pages/CreateBook'
+import EditBook from './pages/EditBook'
+import ShowBook from './pages/ShowBook'
+import Home from './pages/Home'
+import DeleteBook from './pages/DeleteBook'
+
 
 const App = () => {
   return (
     <Routes>
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
-      <Route path='' element={} />
+      <Route path='/books/' element={<Home/>} />
+      <Route path='/books/newBook' element={<CreateBook/>} />
+      <Route path='/books/' element={<ShowBook/>} />
+      <Route path='/books/:id' element={<EditBook/>} />
+      <Route path='/books/:id' element={<DeleteBook/>} />
     </Routes>
   )
 }
